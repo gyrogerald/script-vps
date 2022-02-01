43dcbbbd3e6bc779187815ce8a6eed9e169f1995
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-01 17:34:33 +0800

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -13,8 +13,17 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/geraldyolang11/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/gyroelite"
+exit 0
+fi
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/senowahyu62/scriptvps/main/backup"
+akbarvpn="raw.githubusercontent.com/geraldyolang11/script-vps/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
