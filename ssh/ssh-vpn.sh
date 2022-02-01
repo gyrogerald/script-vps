58b8c77471155078e4686edef8c93c835c202202
Geraldy Olang <97717251+geraldyolang11@users.noreply.github.com>
2022-02-01 18:07:39 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -13,12 +13,14 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geraldyolang11/perizinan/ipvps.txt | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
+IZIN=$( curl https://raw.githubusercontent.com/geraldyolang11/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : https://m.facebook.com/geraldyolang"
+echo -e "${NC}${LIGHT}WhatsApp : 081239935594"
 echo -e "${NC}${LIGHT}Telegram : https://t.me/gyroelite"
 exit 0
 fi
